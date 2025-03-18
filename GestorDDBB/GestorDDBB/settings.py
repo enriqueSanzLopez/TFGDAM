cdcc3797ad9303eecc0f20601fec31adf275b8a9
Enrique Sanz Lopez <enriquesanzlopez@gmail.com>
2025-03-18 20:34:28 +0100

    Preparando conexion con base de datos Postgre

--- GestorDDBB/GestorDDBB/settings.py
+++ GestorDDBB/GestorDDBB/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'GestorDDBB.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test',
+        'USER': 'admin',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
