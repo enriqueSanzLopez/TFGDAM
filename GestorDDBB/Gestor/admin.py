73214e547a42f9a098a7baaa3f2bee033f015855
Enrique Sanz Lopez <enriquesanzlopez@gmail.com>
2025-03-22 21:20:29 +0100

    Preparados los modelos

--- GestorDDBB/Gestor/admin.py
+++ GestorDDBB/Gestor/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from .models import CustomStyle
+from .models import Group
+from .models import User
+from .models import Permission
+from .models import Enumerate
+from .models import Value
 
 # Register your models here.
+@admin.register(CustomStyle)
+class CustomStyleAdmin(admin.ModelAdmin):
+    list_display=('company_name', 'email', 'telephone', 'main_color', 'secondary_color', 'text_color', 'text_size', 'other_code')
+    list_editable=('company_name', 'email', 'telephone', 'main_color', 'secondary_color', 'text_color', 'text_size', 'other_code')
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    search_fields=('desc_group', 'created_at', 'updated_at')
+    list_display=('desc_group', 'created_at', 'updated_at')
+    list_editable=('desc_group')
+    ordering=('-updated_at')
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    search_fields=('name', 'real_name', 'email', 'created_at', 'updated_at')
+    list_filter = ('group')
+    list_display=('name', 'real_name', 'email', 'group', 'created_at', 'updated_at')
+    list_editable=('name', 'real_name', 'email', 'group')
+    ordering=('group', 'name', '-updated_at')
+
+@admin.register(Permission)
+class PermissionAdmin(admin.ModelAdmin):
+    search_fields=('name', 'value', 'created_at', 'updated_at')
+    list_display=('name', 'value', 'order', 'created_at', 'updated_at')
+    list_editable=('name', 'value', 'order')
+    ordering=('name')
+
+@admin.register(Enumerate)
+class EnumerateAdmin(admin.ModelAdmin):
+    search_fields=('name', 'created_at', 'updated_at')
+    list_display=('name', 'created_at', 'updated_at')
+    list_editable=('name')
+    ordering=('name')
+
+@admin.register(Value)
+class ValueAdmin(admin.ModelAdmin):
+    search_fields=('placeholder', 'value', 'created_at', 'updated_at')
+    list_filter = ('enumerate')
+    list_display=('placeholder', 'enumerate', 'value', 'order', 'created_at', 'updated_at')
+    list_editable=('placeholder', 'enumerate', 'value', 'order')
+    ordering=('enumerate', 'order')
--- GestorDDBB/Gestor/models.py
+++ GestorDDBB/Gestor/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 # Create your models here.
+#Modelo para estilos personalizados en la aplicacion
+class CustomStyle(models.Model):
+    main_color=models.CharField(max_length=50, null=True, blank=True)
+    secondary_color=models.CharField(max_length=50, null=True, blank=True)
+    text_color=models.CharField(max_length=50, null=True, blank=True)
+    text_size=models.IntegerField(null=True, blank=True)
+    company_name=models.TextField(null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    telephone=models.TextField(null=True, blank=True)
+    other_code=models.TextField(null=True, blank=True)
+
+#Modelo para indicar grupo
+class Group(models.Model):
+    desc_group=models.TextField(null=False, blank=False)
+    permissions=models.ManyToManyField('Permission', related_name='groups')
+    created_at=models.DateTimeField(null=False, blank=False, auto_now_add=True)
+    updated_at=models.DateTimeField(null=False, blank=False, auto_now=True)
+
+#Modelo para indicar usuario
+class User(models.Model):
+    name=models.CharField(max_length=255, null=False, blank=False, unique=True)
+    real_name=models.TextField(null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    password=models.TextField(null=False, blank=False)
+    def save(self, *args, **kwargs):
+        if self.password:
+            self.password = make_password(self.password)
+        super().save(*args, **kwargs)
+    group=models.ForeignKey(Group, on_delete=models.CASCADE, related_name='users')
+    created_at=models.DateTimeField(null=False, blank=False, auto_now_add=True)
+    updated_at=models.DateTimeField(null=False, blank=False, auto_now=True)
+
+#Modelo para indicar los posibles permisos de la aplicacion
+class Permission(models.Model):
+    name=models.CharField(max_length=50, null=False, blank=False)
+    value=models.TextField(null=True, blank=True)
+    order=models.IntegerField(null=True, blank=True)
+    # groups=models.ManyToManyField('Group', related_name='permissions')
+    created_at=models.DateTimeField(null=False, blank=False, auto_now_add=True)
+    updated_at=models.DateTimeField(null=False, blank=False, auto_now=True)
+
+#Modelo para enumerados
+class Enumerate(models.Model):
+    name=models.CharField(max_length=50, null=False, blank=False, unique=True)
+    created_at=models.DateTimeField(null=False, blank=False, auto_now_add=True)
+    updated_at=models.DateTimeField(null=False, blank=False, auto_now=True)
+
+#Modelo para valores de enumerados
+class Value(models.Model):
+    placeholder=models.TextField(null=True, blank=True)
+    value=models.TextField(null=True, blank=True)
+    order=models.IntegerField(null=True, blank=True)
+    enumerate=models.ForeignKey(Enumerate, on_delete=models.CASCADE, related_name='values')
+    created_at=models.DateTimeField(null=False, blank=False, auto_now_add=True)
+    updated_at=models.DateTimeField(null=False, blank=False, auto_now=True)
