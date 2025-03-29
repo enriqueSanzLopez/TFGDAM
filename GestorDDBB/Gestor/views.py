df06e72fa7810363a82bee92f500d5d07cdc890e
Enrique Sanz Lopez <enriquesanzlopez@gmail.com>
2025-03-29 17:21:14 +0100

    Preparando las rutas de login y main

--- GestorDDBB/Gestor/views.py
+++ GestorDDBB/Gestor/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def login_view(request):
+    #Comprueba si el usuario ha iniciado sesion
+    if request.user.is_authenticated:
+        return redirect('main')
+    else:
+        return render(request, 'login.html')
+    
+@login_required(login_url='/')
+def main_view(request):
+    #Comprueba si el usuario ha iniciado sesion
+    if request.user.is_authenticated:
+        return render(request, 'main.html')
+    else:
+        return redirect('login')
