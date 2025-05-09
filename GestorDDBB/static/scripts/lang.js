2bb97b11c228d232be937d847011c30a466485a1
Enrique Sanz Lopez <enriquesanzlopez@gmail.com>
2025-05-09 23:19:33 +0200

    Arreglos a la seleccion de idioma predeterminado

--- GestorDDBB/static/scripts/lang.js
+++ GestorDDBB/static/scripts/lang.js
@@ -1,4 +1,12 @@
-document.getElementById('lang').addEventListener('change', ()=>{
-    document.cookie="lang="+document.getElementById('lang').value+"; path=/";
-    window.location.href=window.location.href;
+let cookies = document.cookie.split("; ");
+let langCookie = cookies.find(cookie => cookie.startsWith("lang="));
+let langValue = langCookie ? langCookie.split("=")[1] : "es";
+let langSelect = document.getElementById("lang");
+if (langSelect) {
+    langSelect.value = langValue;
+}
+
+document.getElementById('lang').addEventListener('change', () => {
+    document.cookie = "lang=" + document.getElementById('lang').value + "; path=/";
+    window.location.href = window.location.href;
 });
